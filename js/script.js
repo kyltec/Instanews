ab6066cc5e4afcc52189dc7e0c3348330495bf4d
Kyle_Tecson <kyltecs@gmail.com>
2018-10-30 13:41:03 -0700

    move ajax to function

--- js/script.js
+++ js/script.js
@@ -1,41 +1,57 @@
-$(document).ready(function() {
-  // js code
-  $("select").selectric();
+$(document)
+  .ready(function() {
+    // js code
+    $("select").selectric();
+    const $loader = $(".loading").append(
+      "<img src='./images/ajax-loader.gif'>"
+    );
 
-  $("#category").on("change", function(event) {
-    event.preventDefault;
-    let selected = $(this).val();
-    let url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
-    url +=
-      "?" +
-      $.param({
-        "api-key": "1a105db8955341c29606cfcfb8c3c1b2"
-      });
+    $("#category").on("change", function(event) {
+      event.preventDefault;
+      let selected = $(this).val();
 
-    $(".loading").append("<img src='./images/ajax-loader.gif'>");
-    $(".cashe").css("display", "flex");
-    $("header").addClass("nav");
-    $("header").removeClass("flex-container");
-    $("img").addClass("logo");
+      $(".cashe").css("display", "flex");
+      $("header").addClass("nav");
+      $("header").removeClass("flex-container");
+      $("img").addClass("logo");
 
-    $.ajax({
-      method: "GET",
-      url: url,
-      dataType: "JSON"
-    })
-      .done(function(data) {
-        $(".cashe").empty();
-        console.log(data);
+      $loader;
+      getStories(selected);
+    });
+  })
 
-        let sift = data.results.filter(function(value) {
-          return value.multimedia.length;
-        });
-        const slicedArray = sift.slice(0, 12);
+  .always(function() {
+    $("div").removeClass(".loading");
+  })
 
-        $.each(slicedArray, function(key, value) {
-          console.log("ran");
-          $(".cashe").append(
-            `<a href="${value.url}">
+  .fail(function() {
+    $(".cashe").empty();
+    $(".cashe").append("<p> Sorry please try again</p>");
+  });
+
+function getStories(selected) {
+  let url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+  url +=
+    "?" +
+    $.param({
+      "api-key": "1a105db8955341c29606cfcfb8c3c1b2"
+    });
+
+  $.ajax({
+    method: "GET",
+    url: url,
+    dataType: "JSON"
+  }).done(function(data) {
+    $(".cashe").empty();
+
+    let sift = data.results.filter(function(value) {
+      return value.multimedia.length;
+    });
+    const slicedArray = sift.slice(0, 12);
+
+    $.each(slicedArray, function(key, value) {
+      $(".cashe").append(
+        `<a href="${value.url}">
             <div class="article" style=" 
                   background:url(${
                     value.multimedia[4].url
@@ -43,17 +59,7 @@ $(document).ready(function() {
                   >
             <p>${value.abstract}</p>
             </div></a>`
-          );
-        });
-      })
-
-      .always(function() {
-        $("div").removeClass(".loading");
-      })
-
-      .fail(function() {
-        $(".cashe").empty();
-        $(".cashe").append("<p> Sorry please try again</p>");
-      });
+      );
+    });
   });
-});
+}
