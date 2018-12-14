ed080d65f9d31cf4477132996d8eab7220f78362
Kyle_Tecson <kyltecs@gmail.com>
2018-12-13 16:25:40 -0800

    add variables for body in js and padding to text

--- js/script.js
+++ js/script.js
@@ -1,33 +1,22 @@
-$(document)
-  .ready(function() {
-    // js code
-    $("select").selectric();
-    const $loader = $(".loading").append(
-      "<img src='./images/ajax-loader.gif'>"
-    );
+$(document).ready(function() {
+  // js code
+  $("select").selectric();
+  const $loader = $(".loading").append("<img src='./images/ajax-loader.gif'>");
+  let $body = $(".cashe");
 
-    $("#category").on("change", function(event) {
-      event.preventDefault;
-      let selected = $(this).val();
+  $("#category").on("change", function(event) {
+    event.preventDefault;
+    let selected = $(this).val();
 
-      $(".cashe").css("display", "flex");
-      $("header").addClass("nav");
-      $("header").removeClass("flex-container");
-      $("img").addClass("logo");
+    $body.css("display", "flex");
+    $("header").addClass("nav");
+    $("header").removeClass("flex-container");
+    $("img").addClass("logo");
 
-      $loader;
-      getStories(selected);
-    });
-  })
-
-  .always(function() {
-    $("div").removeClass(".loading");
-  })
-
-  .fail(function() {
-    $(".cashe").empty();
-    $(".cashe").append("<p> Sorry please try again</p>");
+    $loader;
+    getStories(selected);
   });
+});
 
 function getStories(selected) {
   let url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
@@ -36,29 +25,40 @@ function getStories(selected) {
     $.param({
       "api-key": "1a105db8955341c29606cfcfb8c3c1b2"
     });
+  let $body = $(".cashe");
 
   $.ajax({
     method: "GET",
     url: url,
     dataType: "JSON"
-  }).done(function(data) {
-    $(".cashe").empty();
+  })
+    .done(function(data) {
+      $body.empty();
 
-    let sift = data.results.filter(function(value) {
-      return value.multimedia.length;
-    });
-    const slicedArray = sift.slice(0, 12);
+      let sift = data.results.filter(function(value) {
+        return value.multimedia.length;
+      });
+      const slicedArray = sift.slice(0, 12);
 
-    for (let value of slicedArray) {
-      const { url, multimedia, abstract } = value;
-      $(".cashe").append(
-        `<a href="${url}">
+      for (let value of slicedArray) {
+        const { url, multimedia, abstract } = value;
+        $body.append(
+          `<a href="${url}">
             <div class="article" style=" 
                   background:url(${multimedia[4].url});background-size:cover;"
                   >
             <p>${abstract}</p>
             </div></a>`
-      );
-    }
-  });
+        );
+      }
+    })
+
+    .always(function() {
+      $("div").removeClass(".loading");
+    })
+
+    .fail(function() {
+      $body.empty();
+      $body.append("<p> Sorry please try again</p>");
+    });
 }
